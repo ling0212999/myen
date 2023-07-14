ec3a0860eb88161299c108456cbf829b051e3639
ling <99333566@qq.com>
2023-07-14 10:32:33 +0800

    init

---
+++ road-of-english-learning/nce/src/main/java/com/nce/nce2/Nce2.java
@@ -0,0 +1,19 @@
+package com.nce.nce2;
+
+/**
+ * 说明：
+ *
+ * @Title : Nce2.java
+ * @Package com.nce.nce2
+ * @See: {@link Nce2}<br/>
+ * Copyright: Copyright (c) 2017<br/>
+ * Company:sany huax witsight team by product
+ * @Author : dail11@sany.com.cn
+ * @Date: 2023-07-14 10:31:42
+ * @version: V1.0
+ */
+public class Nce2 {
+    public static void main(String[] args) {
+        System.out.println("2023年7月14日10:32:05");
+    }
+}
